7cb27144fc14fdaa4ca56d2e99b2a994ad415e9e
nabuller <nabuller@gmail.com>
2021-04-23 23:56:23 -0500

    Completed bubble chart.  Color adjustment wip

--- static/js/app.js
+++ static/js/app.js
@@ -11,10 +11,7 @@ d3.json("samples.json").then((data) => {
 
     d3.select('#selDataset').on('change', function() {
         var newId = eval(d3.select(this).property('value'));
-        // var newData = data.filter(d => {
-        //     return d. === 6
-        //   })
-        // buildDashboard(newData);
+
         var filterIndex = data.names.indexOf(newId.toString());
         console.log(filterIndex);
 
@@ -25,33 +22,64 @@ d3.json("samples.json").then((data) => {
 
     function buildDashboard(metadata, samples){
         console.log(samples);
-
-        // Fill summary demographic information
+        //__________DEMOGRAPHIC INFO PANEL__________//
+        // Populate summary demographic information
         var metadataList = d3.select("#sample-metadata");
         metadataList.html("");
         for (key in metadata){
             metadataList.append("p").text(key +": " + metadata[key]);
         }
 
+        //__________BUBBLE CHART__________//
+        var sample_values = samples.sample_values;
+        var otu_ids = samples.otu_ids;
+        var otu_lables = samples.otu_labels;
+
+        // make hover text slightly more readable
+        for(var i=0; i<otu_lables.length; i++) {
+            otu_lables[i] = otu_lables[i].replaceAll(';','  ');
+        }
+
+        var trace1 = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_lables,
+            mode: 'markers',
+            marker: {
+                size: sample_values.map(d => d * .7),
+                color: otu_ids
+            }
+        };
+            
+        var data = [trace1];
+            
+        var layout = {
+            showlegend: false
+        };
+            
+        Plotly.newPlot('bubble', data, layout);
+
+        //__________BAR CHART__________//
         // set x values for bar graph
-        x = samples.sample_values.slice(0,10);
+        sample_values = sample_values.slice(0,10);
 
         // Set y values for bar graph
-        y = samples.otu_ids.slice(0,10);
-        console.log(y);
-        for(var i=0;i<y.length;i++){
-            y[i]="OTU "+y[i];
+        otu_ids = otu_ids.slice(0,10);
+
+        // Convert y values to string and add OTU as a prefix
+        for(var i=0; i<otu_ids.length; i++) {
+            otu_ids[i] = "OTU "+ otu_ids[i];
         }
 
         //Set hover over text values
-        text = samples.otu_labels.slice(0,10);
+        otu_lables = otu_lables.slice(0,10);
 
         // Create bar chart using x and y data
         var trace = [{
             type: 'bar',
-            x: x,
-            y: y,
-            text: text,
+            x: sample_values,
+            y: otu_ids,
+            text: otu_lables,
             hoverinfo: "text",
             orientation: 'h'
           }];
