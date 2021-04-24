53045a04799053703d868a07f7c9b6d96de5b0f8
nabuller <nabuller@gmail.com>
2021-04-23 23:06:51 -0500

    Completed bar chart and default page load

--- static/js/app.js
+++ static/js/app.js
@@ -24,9 +24,44 @@ d3.json("samples.json").then((data) => {
     });
 
     function buildDashboard(metadata, samples){
+        console.log(samples);
+
+        // Fill summary demographic information
         var metadataList = d3.select("#sample-metadata");
+        metadataList.html("");
         for (key in metadata){
             metadataList.append("p").text(key +": " + metadata[key]);
         }
+
+        // set x values for bar graph
+        x = samples.sample_values.slice(0,10);
+
+        // Set y values for bar graph
+        y = samples.otu_ids.slice(0,10);
+        console.log(y);
+        for(var i=0;i<y.length;i++){
+            y[i]="OTU "+y[i];
+        }
+
+        //Set hover over text values
+        text = samples.otu_labels.slice(0,10);
+
+        // Create bar chart using x and y data
+        var trace = [{
+            type: 'bar',
+            x: x,
+            y: y,
+            text: text,
+            hoverinfo: "text",
+            orientation: 'h'
+          }];
+
+        var layout = {
+            yaxis:{autorange:'reversed'}
+        } 
+        Plotly.newPlot('bar', trace, layout);
     }
+
+    //create default visualization on page load
+    buildDashboard(data.metadata[0], data.samples[0]);
 });
